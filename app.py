0a1b6f18f6647a1f45a7bc75826393dfbd6d2a5b
helios190 <bintangrestu.yk01@gmail.com>
2024-05-27 07:48:59 +0700

    update app

--- app.py
+++ app.py
@@ -78,7 +78,6 @@ def calculate():
         market_return = excess['S&P500'].mean()
 
         alpha_beta_dict = portofolio_calculation.calculate_alpha_beta(excess)
-        excess.to_csv('/Users/bintangrestubawono/Documents/mbm-senpro/FinScope-AI/server')
 
         min_var_portfolios = portofolio_calculation.calculate_minimum_variance_frontier(adj_close_prices)
         max_sharpe_weights, _ = portofolio_calculation.calculate_max_sharpe_ratio(adj_close_prices, risk_free_rate)
